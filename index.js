0f8ebb3635c1e13fbc5017f02e0f9f26cf738601
wara <>
2019-08-17 14:15:35 +0700

    express

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const User = require('./models/user');
 const login = require('./services/authen');
 const calculator = require('./services/calculator');
-const subtract = require('./services/calculator').subtract(3,1);
+const subtract = require('./services/calculator').subtract(3, 1);
 const DatabaseConnect = require('./DatabaseConnect');
+const express = require('express');
 
-let db = new DatabaseConnect();
-db.connect;
-db.disconnect;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ name: 'aum' })
+})
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
+
+// let db = new DatabaseConnect();
+// db.connect;
+// db.disconnect;
 
 
-console.log(subtract);
+// console.log(subtract);
 
-let result = calculator.plus(1,2);
-console.log(result);
+// let result = calculator.plus(1,2);
+// console.log(result);
 
-let addResult = calculator.addOne(10);
-console.log(addResult);
+// let addResult = calculator.addOne(10);
+// console.log(addResult);
 
 // let username = 'aum';
 // console.log(username);
